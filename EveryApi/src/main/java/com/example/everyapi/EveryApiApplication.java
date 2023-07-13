84c23bde10409f1307e64e3de6a9094633cde9f0
CNT_신민정 <smj@clickntouch.co.kr>
2023-07-13 11:02:11 +0900

    make projects

---
+++ EveryApi/src/main/java/com/example/everyapi/EveryApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.everyapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EveryApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EveryApiApplication.class, args);
+    }
+
+}
---
+++ EveryApi/src/test/java/com/example/everyapi/EveryApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.everyapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EveryApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
